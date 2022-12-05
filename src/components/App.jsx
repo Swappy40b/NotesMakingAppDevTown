1ec24ae9947778862f85fd59d33fae7c5c446b08
Swappy40b <74528514+Swappy40b@users.noreply.github.com>
2022-12-05 13:03:42 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Notes />
+
+      <Footer />
+    </div>
+  );
+}
+export default App;
